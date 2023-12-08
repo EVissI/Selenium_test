b3da59f8eda1fe9a8b8c1447a12e8a809d2f339b
EVissI <90708659+EVissI@users.noreply.github.com>
2023-12-08 08:11:40 +0300

    new service add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,5 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
-
-        System.out.println( SeleniumScripts.Sbermarket("79149624234","promokodi500"));
-        //System.out.println(SeleniumScripts.Sbermarket("79089880009","promokodi500"));
-
       }
 }
